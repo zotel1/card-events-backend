9435e6b4b1cd9577357df3664295b6ca8cae225c
zotel1 <zotelsigel@gmail.com>
2025-02-25 22:57:47 -0300

    Initial commit

---
+++ proyect/src/main/java/com/calendar_event/proyect/CalendarEventBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calendar_event.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarEventBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarEventBackendApplication.class, args);
+	}
+
+}
---
+++ proyect/src/test/java/com/calendar_event/proyect/CalendarEventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendar_event.proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarEventBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
