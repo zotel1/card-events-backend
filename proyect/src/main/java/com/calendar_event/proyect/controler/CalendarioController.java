a9e0415929e84645bc18532405e28fb5c255ce18
zotel1 <zotelsigel@gmail.com>
2025-02-27 22:53:45 -0300

    agregamos el controlador CalendarioController

---
+++ proyect/src/main/java/com/calendar_event/proyect/controler/CalendarioController.java
@@ -0,0 +1,13 @@
+package com.calendar_event.proyect.controler;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/calendario")
+@CrossOrigin(origins = "*") // Nos permite las llamadas desde el frontend
+public class CalendarioController {
+
+
+}
