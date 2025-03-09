0f69dfb0a448305421bd90ee53eb46bd280d30cc
zotel1 <zotelsigel@gmail.com>
2025-03-08 21:01:05 -0300

    editamos nuestro event controller

--- proyect/src/main/java/com/calendar_event/proyect/controler/EventController.java
+++ proyect/src/main/java/com/calendar_event/proyect/controler/EventController.java
@@ -1,18 +1,13 @@
 package com.calendar_event.proyect.controler;
 
-import com.auth0.jwt.JWT;
 import com.calendar_event.proyect.model.EventModel;
 import com.calendar_event.proyect.model.UserModel;
 import com.calendar_event.proyect.service.EventService;
-import io.jsonwebtoken.Jwt;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +19,7 @@ public class EventController {
     private EventService eventService;
 
     @PostMapping
-    public ResponseEntity<EventModel> createEvent(@RequestBody EventModel event, @AuthenticationPrincipal JWT jwt) {
+    public ResponseEntity<EventModel> createEvent(@RequestBody EventModel event, @AuthenticationPrincipal Jwt jwt) {
         String userId = jwt.getSubject(); // Obtiene el user_id del token JWT
         event.setUser(new UserModel(userId)); // Asocia el evento al usuario
         EventModel savedEvent = eventService.save(event);
