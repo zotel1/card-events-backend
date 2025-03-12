4c4d3772376bbb6debb8a31655e34c49b383caee
zotel1 <zotelsigel@gmail.com>
2025-03-11 23:48:36 -0300

    actualizamos todo el codigo

--- src/main/java/com/calendar_event/proyect/repository/CalendarioRepository.java
+++ src/main/java/com/calendar_event/proyect/repository/CalendarioRepository.java
@@ -1,4 +1,4 @@
-package com.calendar_event.proyect.repository;
+package com.calendar_event.repository;
 
 import com.calendar_event.proyect.model.EventModel;
 import org.springframework.data.jpa.repository.JpaRepository;
