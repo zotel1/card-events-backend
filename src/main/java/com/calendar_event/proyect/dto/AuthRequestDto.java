4e5711434ccca6cb0b020c5cd0c7f6e2c29cbe29
zotel1 <zotelsigel@gmail.com>
2025-03-13 23:43:05 -0300

    creamos nuestro dto para nuestros usuarios AuthRequestDto

---
+++ src/main/java/com/calendar_event/proyect/dto/AuthRequestDto.java
@@ -0,0 +1,22 @@
+package com.calendar_event.proyect.dto;
+
+public class AuthRequestDto {
+    private String user;
+    private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
