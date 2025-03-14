4f9fc4d0158e59991c439e607c4f16227c0c6e08
zotel1 <zotelsigel@gmail.com>
2025-03-14 00:10:34 -0300

    creamos nuestro filtro JwtRequestFilter

---
+++ src/main/java/com/calendar_event/proyect/filter/JwtRequestFilter.java
@@ -0,0 +1,10 @@
+package com.calendar_event.proyect.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+
+}
