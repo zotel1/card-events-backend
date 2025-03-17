507dd97d342f58cceca66ffb305c0af2085d3df4
zotel1 <zotelsigel@gmail.com>
2025-03-16 22:34:51 -0300

    actualizamos nuestro UserModel

--- src/main/java/com/calendar_event/proyect/model/UserModel.java
+++ src/main/java/com/calendar_event/proyect/model/UserModel.java
@@ -1,31 +1,43 @@
 package com.calendar_event.proyect.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 public class UserModel {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private String id;
 
+    @Column(name = "email", nullable = false)
     private String email; // aca podemos almacenar el email
+
+    @Column(name = "name", nullable = false)
     private String name; //Nombre de nuestro usuario
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private String role;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<EventModel> evenmmts = new ArrayList<>();
 
     public UserModel() {}
 
-    public UserModel(String id, String email, String name, List<EventModel> evenmmts) {
+    public UserModel(String id, String email, String name, List<EventModel> events, String password, String role) {
         this.id = id;
         this.email = email;
         this.name = name;
-        this.evenmmts = evenmmts;
+        this.evenmmts = events;
+        this.password = password;
+        this.role = role;
     }
 
     public UserModel(String userId) {
@@ -62,5 +74,21 @@ public class UserModel {
 
     public void setEvenmmts(List<EventModel> evenmmts) {
         this.evenmmts = evenmmts;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
